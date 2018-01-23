11ace7d2e3cafcab1c1943e631cc5d325807380f
Roman <malaplateroman@gmail.com>
2018-01-23 17:54:57 +0100

    second push

--- send_email.rb
+++ send_email.rb
@@ -1,8 +1,10 @@
 require 'gmail'
 
-
+#define a fonction for Email et le password.
 def send_email_line()
+  #define a variable gmail.
   gmail = Gmail.connect("email","password")
+  #define a variable email with a loop.
     email = gmail.deliver do
       to ws
       subject "Présentation The hacking poject"
@@ -11,25 +13,30 @@ def send_email_line()
         body send_email_text
       end
   end
+  #show email acount loggin.
   puts gmail.logged_in?
+  #desconnecte after work.
   gmail.logout
 end
-
+#define fonction for the texte.
 def send_email_text(cities)
+#text a return
   return"<h2>Bonjour</h2>
-  <p>Je m'appelle Charles Dacquet, je suis co-fondateur de lorganisme The Hacking Project qui propose une formation de dévelopeur web gratuite, ouverte à tous, sans restriction géographique, ni restriction de niveau.<br> Voici le lien de la formation s'appelle The Hacking Project (http://thehackingproject.org/).<br> la foramtion des baser sur la méthode du peer-learning : les étudiants sont répartie en petit groupes ou nous leur proposons la réaliation de projets concrets qui leur sont assignés chaque jours, sur lesquel ils travaillent et cherches des solution . Le projet du jour est d'envoyer des emails à nos élus locaux pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation gratuite.</p>
+  <p>Je m'appelle Charles Dacquay, je suis co-fondateur de lorganisme The Hacking Project qui propose une formation de dévelopeur web gratuite, ouverte à tous, sans restriction géographique, ni restriction de niveau.<br> Voici le lien de la formation s'appelle The Hacking Project (http://thehackingproject.org/).<br> la foramtion des baser sur la méthode du peer-learning : les étudiants sont répartie en petit groupes ou nous leur proposons la réaliation de projets concrets qui leur sont assignés chaque jours, sur lesquel ils travaillent et cherches des solution . Le projet du jour est d'envoyer des emails à nos élus locaux pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation gratuite.</p>
   <p>Nous vous contactons pour vous parler du projet, et vous dire que vous pouvez ouvrir une cellule à #{cities}, où vous pouvez former gratuitement 6 personnes (ou plus), qu'elles soient débutantes, ou confirmées.<br> Le modèle d'éducation de The Hacking Project n'a pas de limite en terme de nombre de moussaillons (c'est comme cela que l'on appelle les élèves), donc nous serions ravis de travailler avec #{cities} !</p>
   <p>Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80</p>"
 end
 
-
+#define a fonction email list
 def list_email()
 session = GoogleDrive::Session.from_config("config.json")
 ws = session.spreadsheet_by_key("1v7XEnpGDtgjgRom3bp7OwzaK99zlUQIfKuW3QdawXBc").worksheets[0]
+#define a lopp with each for the spreadsheet.
 (1..w_sheet.num_rows).each do |list|
 		mail = ws[list, 2]
 		city = ws[list, 1]
     list_email("email","password",mail, city)
  end
 end
+#call the fonction send email.
 send_email_line()
--- speadsheet.rb
+++ speadsheet.rb
@@ -1,40 +1,16 @@
-=begin
-require "google_drive"
-require 'rubygems'
-require 'nokogiri'
-require 'open-uri'
-def get_all_the_urls_of_val_doise_townhalls()
-	page = Nokogiri::HTML(open("https://annuaire-des-mairies.com/val-d-oise.html"))
-	session = GoogleDrive::Session.from_config("config.json")
-	ws = session.spreadsheet_by_key("1v7XEnpGDtgjgRom3bp7OwzaK99zlUQIfKuW3QdawXBc").worksheets[0]
-	cities = ""
-	i = 1
-	page.css('a.lientxt').each do |town|
-		site = "https://annuaire-des-mairies.com" + town['href'][1..-1]
-		cities = town.text
-		ws[(i), 1] = cities
-		ws[(i), 2] = get_the_email_of_a_townhal_from_its_webpage(site)
-		i += 1
-	end
-	ws.save
-end
-def get_the_email_of_a_townhal_from_its_webpage (adresse)
-	page = Nokogiri::HTML(open(adresse))
-	return page.css('td.style27 p.Style22 font')[6].text
-end
-get_all_the_urls_of_val_doise_townhalls()
-=end
+#library who is need by your programme
 require 'google_drive'
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-
+#define a fonction for show a list od website
 def getwebsite()
 	page = Nokogiri::HTML(open("https://annuaire-des-mairies.com/val-d-oise.html"))
 	session = GoogleDrive::Session.from_config("config.json")
 	ws = session.spreadsheet_by_key("1v7XEnpGDtgjgRom3bp7OwzaK99zlUQIfKuW3QdawXBc").worksheets[0]
 	cities = ""
 	i = 1
+	#define a loop for list of emails
 	page.css('a.lientxt').each do |town|
 		site = "https://annuaire-des-mairies.com" + town['href'][1..-1]
 		cities = town.text
@@ -44,8 +20,10 @@ def getwebsite()
 	end
 	ws.save
 end
+#define fonction for show email
 def getemail (adresse)
 	page = Nokogiri::HTML(open(adresse))
 	return page.css('td.style27 p.Style22 font')[6].text
 end
+#call th fonction get website
 getwebsite()
